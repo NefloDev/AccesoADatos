be7a4baead0510c0ff5ad5b6afa5af9d9c505b79
Alejandro Nebot <alneflo27@gmail.com>
2023-11-29 18:48:10 +0100

    Respuesta a la pregunta sobre el id autogenerado de MongoDB

--- Ejercicios/UD3/AWSMongoDB/src/main/java/Main.java
+++ Ejercicios/UD3/AWSMongoDB/src/main/java/Main.java
@@ -5,6 +5,12 @@ public class Main {
         CRUDOperationsPilot.showPilotsOlderThan(30);
 
         CRUDOperationsPilot.stopConnection();
+
+        //Crear un id autogenerado personalizado desde la base de datos de MongoDB puede ayudar a la gestión de elementos
+        //pero también puede ser un problema de eficiencia si se tiene una base de datos muy grande
+
+        //Si realmente seguir usando el id autogenerado de MongoDB no supone ningun inconveniente al gestionar la Base de datos
+        //puede dejarse como está para mejorar el rendimiento
     }
 
 }
