33cad95e10df909d0a7a30cd6fea8625bfaab760
Alejandro Nebot <alneflo27@gmail.com>
2024-01-11 19:22:11 +0100

    Metodos HTTP en API REST

--- Ejercicios/UD3/EjercicioAPIRest/src/main/java/org/example/ejercicioapirest/repository/DriverRepository.java
+++ Ejercicios/UD3/EjercicioAPIRest/src/main/java/org/example/ejercicioapirest/repository/DriverRepository.java
@@ -4,6 +4,10 @@ import org.example.ejercicioapirest.model.Driver;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface DriverRepository extends JpaRepository<Driver, Long> {
+    //Método reconocido por Spring
+    Optional<Driver> findByCodeIgnoreCase(String code);
 }
--- Ejercicios/UD3/EjercicioAPIRest/src/main/java/org/example/ejercicioapirest/service/DriverServiceImpl.java
+++ Ejercicios/UD3/EjercicioAPIRest/src/main/java/org/example/ejercicioapirest/service/DriverServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DriverServiceImpl implements DriverService {
@@ -14,9 +15,20 @@ public class DriverServiceImpl implements DriverService {
     public DriverServiceImpl(DriverRepository repo) {
         this.repo = repo;
     }
-
     @Override
     public List<Driver> getAllDrivers() {
         return repo.findAll();
+    }
+    @Override
+    public Optional<Driver> getDriverByCode(String code) {
+        return repo.findByCodeIgnoreCase(code);
+    }
+    @Override
+    public Driver save(Driver driver) {
+        return repo.save(driver);
+    }
+    @Override
+    public void deleteByCode(String code) {
+        repo.delete(repo.findByCodeIgnoreCase(code).get());
     }
 }
